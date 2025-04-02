09db4279b175c28d99ca118dbdc91ef90c4d0fe8
Ezequiel Leonardo Castaño <14986783+ELC@users.noreply.github.com>
2025-04-01 22:25:35 -0300

    feat(book): update configuration and add setup script
    - Include Pipfile and Pipfile.lock in the manifest.
    - Update repository URLs and base URL in the book config.
    - Add setup.py for package management and distribution.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "core"
+PACKAGE_VERSION = "0.1.0"
+AUTHOR = "Ezequiel Leonardo Castaño"
+AUTHOR_EMAIL = "-"
+DESCRIPTION = "Course material to use along with the classes"
+URL = "https://github.com/ELC/sistemas-de-gestion"
+LICENSE = "MIT"
+PACKAGE_DIR = "book/chapters/core"
+LONG_DESCRIPTION = Path("README.md").read_text(encoding="utf-8")
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url=URL,
+    license=LICENSE,
+    packages=["core"] + [f"core.{p}" for p in find_packages(where=PACKAGE_DIR)],
+    package_dir={"core": PACKAGE_DIR},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    setup_requires='setuptools-pipfile',
+    use_pipfile=True,
+)
